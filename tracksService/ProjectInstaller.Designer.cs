2640a75f683146a4267580b9342c721ddfb07f43
Scott <scott.mac.web@gmail.com>
2013-03-26 14:09:48 -0700

    windows service for tracks app host

---
+++ tracksService/ProjectInstaller.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace tracksService
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tracksServiceProcessInstaller = new System.ServiceProcess.ServiceProcessInstaller();
+            this.tracksServiceInstaller = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // tracksServiceProcessInstaller
+            // 
+            this.tracksServiceProcessInstaller.Account = System.ServiceProcess.ServiceAccount.LocalService;
+            this.tracksServiceProcessInstaller.Password = null;
+            this.tracksServiceProcessInstaller.Username = null;
+            // 
+            // tracksServiceInstaller
+            // 
+            this.tracksServiceInstaller.Description = "Tracks REST API Service Host";
+            this.tracksServiceInstaller.DisplayName = "TracksService";
+            this.tracksServiceInstaller.ServiceName = "TracksService";
+            this.tracksServiceInstaller.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.tracksServiceProcessInstaller,
+            this.tracksServiceInstaller});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller tracksServiceProcessInstaller;
+        private System.ServiceProcess.ServiceInstaller tracksServiceInstaller;
+    }
+}
---
+++ tracksService/TracksService.cs
@@ -0,0 +1,55 @@
+﻿using ServiceStack.Configuration;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using tracksStackd;
+
+namespace tracksService
+{
+    public partial class TracksService : ServiceBase
+    {
+        private const string _eventLogSource = "TracksRest";
+        public AppHost Host { get; set; }
+        public TracksService()
+        {
+            InitializeComponent();
+
+            if (!System.Diagnostics.EventLog.SourceExists(_eventLogSource))
+            {
+                System.Diagnostics.EventLog.CreateEventSource(_eventLogSource, "TracksRestAPI");
+            }
+            eventLog.Source = _eventLogSource;
+            eventLog.Log = "Application";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog.WriteEntry("Tracks Rest API host starting");
+
+            var settings = new AppSettings();
+
+            Host = new AppHost();
+            Host.Init();
+            Host.InitDb();
+            Host.Start(settings.GetString("restBaseAddress"));
+        }
+
+        protected override void OnStop()
+        {
+            eventLog.WriteEntry("Tracks Rest API host stopping");
+            Host.Stop();
+        }
+
+        protected override void OnShutdown()
+        {
+            Host.Stop();
+            base.OnShutdown();
+        }
+    }
+}
